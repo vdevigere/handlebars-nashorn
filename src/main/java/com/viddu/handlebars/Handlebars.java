eb7121c99beca70e2cbedbfa3be42dc5403c7125
Viddu Devigere <vdevigere@gmail.com>
2014-07-14 21:31:05 -0700

    Adding registerPartial support

--- src/main/java/com/viddu/handlebars/Handlebars.java
+++ src/main/java/com/viddu/handlebars/Handlebars.java
@@ -7,12 +7,12 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.nio.charset.StandardCharsets;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
-import javax.script.Invocable;
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
-import javax.script.ScriptException;
+import javax.script.*;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,8 +24,13 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 public class Handlebars {
 
     private final ScriptEngine engine;
-    
+
     private final ObjectMapper mapper;
+
+    // This is required as using the mapper with
+    // SerializationFeature.WRAP_ROOT_VALUE will result in the class name of map
+    // (eg:- HashMap) being a part of the JSON string
+    private final ObjectMapper mapperForMap;
 
     private final Logger logger = LoggerFactory.getLogger(Handlebars.class);
 
@@ -34,7 +39,9 @@ public class Handlebars {
         this.engine = engineManager.getEngineByName("nashorn");
         engine.eval(reader);
         engine.eval("var renderFunction = function(template, contextJSON){var context = JSON.parse(contextJSON); return Handlebars.compile(template)(context);}");
+        engine.eval("var registerPartialFunction = function(partialName, partialText){return Handlebars.registerPartial(partialName, partialText);}");
         this.mapper = new ObjectMapper();
+        this.mapperForMap = new ObjectMapper();
         this.mapper.enable(SerializationFeature.WRAP_ROOT_VALUE);
     }
 
@@ -55,7 +62,12 @@ public class Handlebars {
 
     public String render(String template, Object context) throws NoSuchMethodException, ScriptException,
             JsonProcessingException {
-        String contextJson = mapper.writeValueAsString(context);
+        String contextJson;
+        if (context instanceof Map || context instanceof List) {
+            contextJson = mapperForMap.writeValueAsString(context);
+        } else {
+            contextJson = mapper.writeValueAsString(context);
+        }
         logger.debug("Context JSON={}", contextJson);
         return render(template, contextJson);
     }
@@ -67,5 +79,12 @@ public class Handlebars {
         String renderedOutput = render(template, context);
         scanner.close();
         return renderedOutput;
+    }
+    
+    public void registerPartial(String partialName, InputStream partial) throws NoSuchMethodException, ScriptException{
+        Scanner scanner = new Scanner(partial, StandardCharsets.UTF_8.name());
+        String partialText = scanner.useDelimiter("\\A").next();
+        Invocable registerPartial = (Invocable) engine;
+        registerPartial.invokeFunction("registerPartialFunction", partialName, partialText);
     }
 }
